f5f8a73a68942b30e956590539d103fab2c890c8
dommurphy155 <218424478+dommurphy155@users.noreply.github.com>
2025-07-14 23:09:05 +0100

    g

--- bot_runner.py
+++ bot_runner.py
@@ -1,12 +1,19 @@
 import asyncio
 import logging
+import nest_asyncio
 from telegram_bot import TelegramBot
 
 logging.basicConfig(level=logging.INFO)
+
+nest_asyncio.apply()
 
 async def main():
     bot = TelegramBot()
     await bot.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        pass
